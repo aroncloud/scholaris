80a31421c1c238f2aa113cbfb0e7ae8f12ceea25
STEVE LOIC NGANDA ONANA <steveloicnganda@gmail.com>
2025-08-25 03:10:35 +0100

    .

--- src/actions/requestSubmissionActions.ts
+++ src/actions/requestSubmissionActions.ts
@@ -10,7 +10,7 @@ export async function searchStudentByMatricule(matricule: string) {
             `${process.env.APPLICATION_WORKER_ENDPOINT}/api/public/student-applications/${matricule}`,
             {
                 headers: {
-                    "X-API-Key": process.env.PUBLIC_API_KEY,
+                    "X-API-Key": process.env.X_API,
                 },
             }
         );
@@ -36,7 +36,7 @@ export async function submitAdmissionRequest(formData: CreateApplicantRequest, a
             formData,
             {
                 headers: {
-                    "X-API-Key": process.env.PUBLIC_API_KEY,
+                    "X-API-Key": process.env.X_API,
                 },
             }
         );
