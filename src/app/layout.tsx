baf392330322365dc3a9cd25db9033d077e16f43
STEVE LOIC NGANDA ONANA <steveloicnganda@gmail.com>
2025-10-21 12:56:59 +0100

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,35 +1,95 @@
-"use client"; // <-- Mark the layout as a Client Component
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Space_Grotesk } from 'next/font/google';
+import { Toaster } from "@/components/ui/sonner"
+import localFont from 'next/font/local'
+// import './globals.css';
 
-import { Space_Grotesk } from "next/font/google";
-import { Toaster } from "@/components/ui/sonner";
-import localFont from "next/font/local";
-
-import { SidebarProvider } from "@/context/SidebarContext";
-import { ThemeProvider } from "@/context/ThemeContext";
-import { AppProgressProvider } from "@bprogress/next";
-import { AuthProvider } from "@/components/auth/AuthContext";
+import { SidebarProvider } from '@/context/SidebarContext';
+import { ThemeProvider } from '@/context/ThemeContext';
+import BarProvider from "@/context/BarContext";
+import ToastProvider from '@/components/ui/ToastProvider';
 
 const SpaceGrotesk = Space_Grotesk({
   subsets: ["latin"],
 });
 
-// If you want, you can uncomment and use your Satoshi font
-// const myFont = localFont({ src: './fonts/Satoshi-Variable.woff2' });
+// export const satoshi = localFont({
+//   src: [
+//     {
+//       path: "./Satoshi-Regular.otf",
+//       weight: "400",
+//       style: "normal",
+//     },
+//     {
+//       path: "./Satoshi-Italic.otf",
+//       weight: "400",
+//       style: "italic",
+//     },
+//     {
+//       path: "./Satoshi-Light.otf",
+//       weight: "300",
+//       style: "normal",
+//     },
+//     {
+//       path: "./Satoshi-LightItalic.otf",
+//       weight: "300",
+//       style: "italic",
+//     },
+//     {
+//       path: "./Satoshi-Medium.otf",
+//       weight: "500",
+//       style: "normal",
+//     },
+//     {
+//       path: "./Satoshi-MediumItalic.otf",
+//       weight: "500",
+//       style: "italic",
+//     },
+//     {
+//       path: "./Satoshi-Bold.otf",
+//       weight: "700",
+//       style: "normal",
+//     },
+//     {
+//       path: "./Satoshi-BoldItalic.otf",
+//       weight: "700",
+//       style: "italic",
+//     },
+//     {
+//       path: "./Satoshi-Black.otf",
+//       weight: "900",
+//       style: "normal",
+//     },
+//     {
+//       path: "./Satoshi-BlackItalic.otf",
+//       weight: "900",
+//       style: "italic",
+//     },
+//   ],
+//   variable: "--font-satoshi",
+//   display: "swap",
+// });
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+// const myFont = localFont({
+//   src: './fonts/Satoshi-Variable.woff2',
+// })
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <html lang="en">
-      <body className={`${SpaceGrotesk.className} dark:bg-gray-900`}>
-        <AppProgressProvider>
-          <ThemeProvider>
-            <ToastProvider>
-              <AuthProvider>
-                <SidebarProvider>{children}</SidebarProvider>
-              </AuthProvider>
-            </ToastProvider>
-          </ThemeProvider>
-          <Toaster />
-        </AppProgressProvider>
+      <body className={`${SpaceGrotesk.className} dark:bg-gray-900 bg-gray-50`}>
+      <BarProvider>
+        <ThemeProvider>
+          <ToastProvider>
+            <SidebarProvider>{children}</SidebarProvider>
+          </ToastProvider>
+        </ThemeProvider>
+        <Toaster />
+      </BarProvider>
       </body>
     </html>
   );
